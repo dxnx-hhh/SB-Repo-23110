157070199a8e55a0f5d098659e4137605108deb4
dxnx-hhh <dxnxhb@gmail.com>
2023-11-03 19:31:47 +0900

    Initial commit

---
+++ src/test/java/com/example/selfcoding/SelfcodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.selfcoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfcodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
