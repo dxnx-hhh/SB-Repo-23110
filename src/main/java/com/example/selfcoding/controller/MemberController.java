3bb3e22b1631fd120a92465172fe32736c21ad80
dxnx-hhh <dxnxhb@gmail.com>
2023-11-09 18:22:07 +0900

    08장: 게시글 삭제(Delete)_오타 수정

--- src/main/java/com/example/selfcoding/controller/MemberController.java
+++ src/main/java/com/example/selfcoding/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 
@@ -85,7 +86,23 @@ public class MemberController {
        return "redirect:/members/" + memberEntity.getId();
    }
 
+    @GetMapping("/members/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr) {
+        log.info("삭제 요청 들어옴");  //delete 메서드 작동여부 확인
 
+        //1. 삭제할 대상 가져오기
+        Member target = memberRepository.findById(id).orElse(null);
+        log.info(target.toString());
+
+        //2. 대상 엔티티 삭제하기
+        if(target != null) {
+            memberRepository.delete(target);
+            rttr.addFlashAttribute("msg", "삭제됨");
+        }
+
+        //3. 결과 페이지로 리다이렉트
+        return "redirect:/members";
+    }
 
 
 
